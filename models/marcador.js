a1f54bb0ecab33dae2f56ae6b9fdb6ea6ad235e1
Daniel Mateu <danielmateu86@gmail.com>
2023-05-15 09:40:24 +0200

    models marcadores + marcador

---
+++ models/marcador.js
@@ -0,0 +1,12 @@
+
+
+class Marcador {
+    constructor(id, nombre, lat, lng) {
+        this.id = id;
+        this.nombre = nombre;
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
+
+module.exports = Marcador;
